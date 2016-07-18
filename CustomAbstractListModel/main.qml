8339f0748d62c76fcf9d8133df550fba12d7fba6
Mateusz Midor <mateusz>
2016-07-18 11:01:22 +0200

    Added Add/Remove/Update row examples to CustomAbstractListModel

--- CustomAbstractListModel/main.qml
+++ CustomAbstractListModel/main.qml
@@ -2,10 +2,10 @@ import QtQuick 2.3
 import QtQuick.Window 2.2
 import QtQuick.Controls 1.4
 import TunerListModel 1.0
-
+import QtQuick.Layouts 1.1
 Window {
     visible: true
-    width: 350; height: 200
+    width: 550; height: 200
 
 
     // define AbstractListModel implementation (ChannelName, Frequency roles)
@@ -13,23 +13,53 @@ Window {
         id: tunerModel
     }
 
-    TableView {
+    ColumnLayout{
         anchors.fill: parent
-        model: tunerModel
+        TableView {
+            anchors.fill: parent
+            model: tunerModel
 
-        TableViewColumn {
-            title: "Name"
-            role: "Name"
+            TableViewColumn {
+                title: "Name"
+                role: "Name"
+            }
+
+            TableViewColumn {
+                title: "Freq"
+                role: "Frequency"
+            }
+
+            onClicked: {
+                console.log(tunerModel.get(row, TunerListModel.Name) + " - " + tunerModel.get(row, TunerListModel.Frequency));
+            }
         }
 
-        TableViewColumn {
-            title: "Freq"
-            role: "Frequency"
-        }
+        RowLayout {
+            anchors.bottom: parent.bottom
+            Button {
+                text: "Add First"
+                onClicked: tunerModel.addFirst()
+            }
 
-        onClicked: {
-            console.log(tunerModel.get(row, 0) + " - " + tunerModel.get(row, 1));
+            Button {
+                text: "Add Last"
+                onClicked: tunerModel.addLast()
+            }
+
+            Button {
+                text: "Update Middle"
+                onClicked: tunerModel.updateMiddle()
+            }
+
+            Button {
+                text: "Remove First"
+                onClicked: tunerModel.removeFirst()
+            }
+
+            Button {
+                text: "Remove Last"
+                onClicked: tunerModel.removeLast()
+            }
         }
     }
 }
-
--- CustomAbstractListModel/tunerlistmodel.h
+++ CustomAbstractListModel/tunerlistmodel.h
@@ -4,21 +4,28 @@
 #include <QObject>
 #include <QDebug>
 #include <QAbstractListModel>
+#include <QVector>
+#include <QTime>
+
+struct Channel {
+    QString name;
+    QString frequency;
+};
+
 class TunerListModel : public QAbstractListModel {
     Q_OBJECT
 
-    // define some constant channel data
-    static const int NUM_CHANNELS  = 3;
-    const QString DATA[NUM_CHANNELS][2] = {{"70's", "87.7 MHz"}, {"80's", "90.1 MHz"}, {"90's", "97.7 MHz"}};
-
+    // define some channel data
+    QVector<Channel> channels {{"70's", "87.7 MHz"}, {"80's", "90.1 MHz"}, {"90's", "97.7 MHz"}};
+public:
     // roles will be columns in the QML TableView
     enum Roles {
         Name = Qt::UserRole + 1,
         Frequency
     };
+    // export enum to QML so it is accessible like: TunerListModel.Frequency
+    Q_ENUM(Roles)
 
-
-public:
     // QAbstractListModel pure-virtual function 1.
     QHash<int, QByteArray> roleNames() const override {
         QHash<int, QByteArray> roles;
@@ -28,28 +35,69 @@ public:
     }
 
     // QAbstractListModel pure-virtual function 2.
-    int rowCount(const QModelIndex &/* index */) const override {
-        return NUM_CHANNELS;
+    int rowCount(const QModelIndex &/* index */ = QModelIndex()) const override {
+        return channels.length();
     }
 
     // QAbstractListModel pure-virtual function 3.
     Q_INVOKABLE QVariant data(const QModelIndex& modelIndex, int role) const override {
         int row = modelIndex.row();
+        return get(row, role);
+    }
+
+    // data getter
+    Q_INVOKABLE QVariant get(int row, int role) const {
         switch (role) {
         case Name:
-            return DATA[row][0];
+            return channels[row].name;
         case Frequency:
-            return DATA[row][1];
+            return channels[row].frequency;
         default:
             qDebug() << "invalid role " << role << endl;
             return QVariant();
         }
     }
 
-    Q_INVOKABLE QString get(int row, int role) {
-        return DATA[row][role];
+    // add row at first position
+    Q_INVOKABLE void addFirst() {
+        beginInsertRows(QModelIndex(), 0, 0);
+        Channel c {QString("First Ch."), QString("First frq")};
+        channels.insert(0, c);
+        endInsertRows();
     }
 
+    // add row at last position
+    Q_INVOKABLE void addLast() {
+        auto rowcount = rowCount();
+        beginInsertRows(QModelIndex(), rowcount, rowcount);
+        Channel c {QString("Last Ch."), QString("Last frq")};
+        channels.insert(rowcount, c);
+        endInsertRows();
+    }
+
+    // remove row at first position
+    Q_INVOKABLE void removeFirst() {
+        beginRemoveRows(QModelIndex(), 0, 0);
+        channels.remove(0);
+        endRemoveRows();
+    }
+
+    // remove row at last position
+    Q_INVOKABLE void removeLast() {
+        auto rowcount = rowCount();
+        beginRemoveRows(QModelIndex(), rowcount -1, rowcount -1);
+        channels.remove(rowcount -1);
+        endRemoveRows();
+    }
+
+    //  update row at middle position to current time
+    Q_INVOKABLE void updateMiddle() {
+        auto middle_idx = channels.length()/2;
+        channels[middle_idx].name = QTime::currentTime().toString();
+        QModelIndex topleft = createIndex(middle_idx, 0, nullptr); // row, column, some data pointer (?)
+        QModelIndex bottomright = createIndex(middle_idx, 0, nullptr);
+        emit dataChanged(topleft, bottomright);
+    }
 };
 
 #endif // TUNERLISTMODEL
